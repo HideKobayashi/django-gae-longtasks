8c3451d48295e005c108c47ddb7a68b65d254b9a
Hideyuki KOBAYASHI <hidekobayashi512.d4c@gmail.com>
2022-08-20 08:36:08 +0000

    URL /bqapp/jobmng added

---
+++ myproject/bqapp/tests/test_urls.py
@@ -0,0 +1,38 @@
+from django.urls import resolve, reverse
+from django.test import RequestFactory
+
+from bqapp.views import JobMngView
+
+
+class TestUrls:
+    """ URLのテスト
+    """
+    def test_url_to_view_resolve(self):
+        """正常系
+
+        条件: url: "/bqapp/jobmng"
+        期待値: ビュー関数のクラスが bqapp.views.JobMngView
+        """
+        url = "/bqapp/jobmng"
+        expect_view_class = JobMngView
+
+        actual = resolve(url)
+
+        print(f"actual: {actual}", end="| ")
+        print(f"actual.func: {actual.func}", end="| ")
+        print(f"actual.func.view_class: {actual.func.view_class}", end="| ")
+        assert expect_view_class == actual.func.view_class 
+
+    def test_viewname_to_url_reverse(self):
+        """正常系
+
+        条件: viewname が "jobmng"
+        期待値: urlが "/bqapp/jobmng"
+        """
+        viewname = "jobmng"
+        expect_url = "/bqapp/jobmng"
+
+        actual = reverse(viewname)
+
+        print(f"actual: {actual}", end="| ")
+        assert expect_url == actual
--- myproject/bqapp/views.py
+++ myproject/bqapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+
+class JobMngView(TemplateView):
+    """ジョブ管理のための View
+    """
+    def get(self, request, *args, **kwargs):
+        """GET リクエストを受け取ったときに呼ばれるメソッド
+        """
+        response = {}
+        response["message"] = "Hello"
+        return JsonResponse(response)
