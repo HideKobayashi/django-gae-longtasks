509dadd78e04b80327c9b6b7b40450d102845e47
Hideyuki KOBAYASHI <hidekobayashi512.d4c@gmail.com>
2022-08-21 05:35:02 +0000

    BqScript.urge_process_to_go_forward added

--- myproject/bqapp/bqreq.py
+++ myproject/bqapp/bqreq.py
@@ -1,7 +1,13 @@
 """BigQuery Requests
 """
+# import time
 from google.cloud import bigquery
-import time
+
+
+class ParamError(Exception):
+    """パラメータに不備があるときに上げる例外
+    """
+
 
 project = "bigquery-poc-220606"
 location = None
@@ -9,7 +15,9 @@ location = None
 
 
 def query_sleep(sleep_time: int = 5):
-    interval = 1
+    rdict = {"job_id": None, "state": None}
+    rdict["project"] = project
+    rdict["location"] = location
     client = bigquery.Client(project=project, location=location)
     query_job = client.query(
         f"CALL `bigquery-poc-220606.tools_tokyo.sleep`({sleep_time})"
@@ -17,20 +25,83 @@ def query_sleep(sleep_time: int = 5):
 
     job_id = query_job.job_id
 
-    while query_job.running():
-        print(f"job_id: {job_id}, Job state: {query_job.state}")
-        time.sleep(interval)
+    # while query_job.running():
+    #     interval = 1
+    #     print(f"job_id: {job_id}, Job state: {query_job.state}")
+    #     time.sleep(interval)
 
-    print(f"job_id: {job_id}, Job state: {query_job.state}")
+    rdict["job_id"] = job_id
+    rdict["state"] = query_job.state
+    # print(f"job_id: {job_id}, Job state: {query_job.state}")
+    return rdict
+
+
+def get_job_state(job_id: str):
+    rdict = {"job_id": None, "state": None}
+    rdict["project"] = project
+    rdict["location"] = location
+    client = bigquery.Client(project=project, location=location)
+    query_job = client.get_job(job_id, project=project, location=location)
+    return query_job.state
 
 
 class BqScript:
     """BigQuery Script
     """
-    def __init__(self):
+    main_job_id = ""
+    main_task_name = ""
+    progress_dict = {}
+
+    def __init__(self, main_job_id: str, main_task_name: str):
+        """初期化
         """
-        """
+        self.main_job_id = main_job_id
+        self.main_task_name = main_task_name
+        self.tasks_dict = {
+            "task1_begin": {"func": self.s_task1_begin, "next": "task2_mid"},
+            "task2_mid": {"func": self.s_task2_mid, "next": "task3_mid"},
+            "task3_mid": {"func": self.s_task3_mid, "next": "task4_mid"},
+            "task4_mid": {"func": self.s_task4_mid, "next": "task5_end"},
+            "task5_end": {"func": self.s_task5_end, "next": None},
+        }
         return
+
+    def urge_process_to_go_forward(self, task_name: str, job_id: str):
+        """処理を進めるよう要請する
+        """
+        next_job_id = None
+        tasks = self.tasks_dict
+        task = tasks.get(task_name, None)
+        if task is None:
+            msg = f"No task defined in the tasks_dict: {tasks}"
+            raise ParamError(msg)
+        # task_func = task["func"]
+        next_task_name = task["next"]
+        if next_task_name is None:
+            next_task_func = None
+        else:
+            next_task_func = tasks[next_task_name]["func"]
+
+        # job_id のジョブが終わっているか確認する
+        state = get_job_state(job_id)
+
+        if state == "RUNNING":
+            message = f"Current task state is {state}. (job_id: {job_id}, task_name: {task_name}) "
+        # ジョブが終わっていたら次のジョブをスタートする
+        elif state == "DONE":
+            if next_task_func is None:
+                # 一連の全ての子タスクが終了したことを知らせる
+                message = f"Final process state is {state}. (job_id: {job_id}, task_name: {task_name})"
+            else:
+                # 次のタスクを実行して次のタスクの job_id を得る
+                rdict = next_task_func()
+                next_job_id = rdict.get("job_id", None)
+                message = f"Next job is started. (next_job_id: {next_job_id}, next_task_name: {next_task_name}"
+        else:
+            message = f"Current task state is {state}. (job_id: {job_id}, task_name: {task_name})"
+
+        # スタートした次のジョブの job_id を返す
+        return next_job_id, message
 
     def s_task1_begin(self):
         """最初のタスク
--- myproject/bqapp/tests/test_bqreq.py
+++ myproject/bqapp/tests/test_bqreq.py
@@ -1,7 +1,10 @@
 """Test bqapp
 """
+from unittest import mock
 import pytest
 from pytest import param as pp
+
+from google.cloud import bigquery
 
 from bqapp.bqreq import BqScript
 
@@ -20,14 +23,86 @@ class TestBqScript_NoMock:
         print(f"actual: {actual}", end="| ")
 
 
-# class TestBqScript:
-#     """BqScript の単体テスト
-#     """
-#     def test_s_tesk1(self):
-#         """ メソッド s_task1
-#         """
-#         bqscript = BqScript()
+def mocked_query_job(job_id: str) -> bigquery.QueryJob:
+    """モックしたQueryJob
 
-#         actual = bqscript.s_task1_begin()
+    job_id: "jobid_running", "jobid_done", "jobid_fail" のいずれか
+    """
+    qj = mock.Mock()
+    if job_id == "jobid_running":
+        running_ret, state, result_ret = True, "RUNNING", None
+    elif job_id == "jobid_done":
+        running_ret, state, result_ret = False, "DONE", [('a', 1), ('b', 2)]
+    elif job_id == "jobid_fail":
+        running_ret, state, result_ret = False, "FAIL", None
+    else:
+        running_ret, state, result_ret = False, "UNKNOWN", None
+    qj.job_id = job_id
+    qj.running.return_value = running_ret
+    qj.state = state
+    qj.result.return_value = result_ret
+    return qj
 
-#         print(f"actual: {actual}", end="| ")
+
+@pytest.fixture
+def mocked_query_job_running():
+    return mocked_query_job("jobid_running")
+
+
+@pytest.fixture
+def mocked_query_job_done():
+    return mocked_query_job("jobid_done")
+
+
+# @pytest.fixture
+# def mocked_query_job_done_run_3_times():
+#     qj = mock.Mock()
+#     qj.job_id = "jobid_done_run_3_times"
+#     qj.running.side_effect = [True, True, True, False]
+#     qj.result.return_value = [('a', 1), ('b', 2)]
+#     qj.state = "DONE"
+#     return qj
+
+
+class TestBqScript:
+    """BqScript の単体テスト
+    """
+    def test_s_tesk1(self):
+        """ メソッド s_task1
+        """
+        main_job_id = "main_jobid"
+        main_task_name = "main_task"
+        job_id = "jobid_done"
+        expect_job_id = job_id
+        with mock.patch('bqapp.bqreq.bigquery.Client', autospec=True) as mock_client:
+            mock_client().query.return_value = mocked_query_job(job_id)
+            bqscript = BqScript(main_job_id, main_task_name)
+            actual = bqscript.s_task1_begin()
+
+        print(f"actual: {actual}", end="| ")
+        assert actual["job_id"] == expect_job_id
+
+    @pytest.mark.parametrize('task_name, expect_message', [
+        pp("task1_begin", "Next job is started", id="task1_begin"),
+        pp("task2_mid", "Next job is started", id="task2_mid"),
+        pp("task3_mid", "Next job is started", id="task3_mid"),
+        pp("task4_mid", "Next job is started", id="task4_mid"),
+        pp("task5_end", "Final process state is", id="task5_end"),
+    ])
+    def test_urge_process_to_go_forward(self, task_name, expect_message):
+        """ メソッド urge_process_to_go_forward
+        """
+        main_job_id = "main_jobid"
+        main_task_name = "main_task"
+        job_id = "jobid_done"
+        next_job_id = "jobid_running"
+
+        with mock.patch('bqapp.bqreq.bigquery.Client', autospec=True) as mock_client:
+            mock_client().get_job.return_value = mocked_query_job(job_id)
+            mock_client().query.return_value = mocked_query_job(next_job_id)
+            bqscript = BqScript(main_job_id, main_task_name)
+            job_info, message = bqscript.urge_process_to_go_forward(task_name, job_id)
+        actual = message
+
+        print(f"actual: {actual}", end="| ")
+        assert actual.startswith(expect_message)
